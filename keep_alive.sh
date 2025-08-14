019df8942937177c5591b170d83e46f024ed5910
Aff245 <wudi250305@gmail.com>
2025-08-14 08:13:53 +0700

    Delete keep_alive.sh

--- keep_alive.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# Mencegah Android tidur selama operasi BDI
-termux-wake-lock
-termux-notification --title "🤖 BDI Agent Active" --content "FMAA Enterprise Running"
-
-
