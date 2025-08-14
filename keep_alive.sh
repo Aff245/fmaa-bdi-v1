91d56f7e7abd8266278f55f3314526adef07bb46
Aff245 <wudi250305@gmail.com>
2025-08-14 08:08:22 +0700

    Add files via upload

---
+++ keep_alive.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Mencegah Android tidur selama operasi BDI
+termux-wake-lock
+termux-notification --title "🤖 BDI Agent Active" --content "FMAA Enterprise Running"
+
+
